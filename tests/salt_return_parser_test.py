61015f0d4b4892a2d66cddd43b526be52c94355f
isingh <isingh@dandb.com>
2017-12-06 16:01:42 -0800

    DEVOPS-10055: added unit test for parsing possible highstate failures

--- tests/salt_return_parser_test.py
+++ tests/salt_return_parser_test.py
@@ -1,12 +1,20 @@
 import unittest
+import os
 from mock import patch
 from fakeredis import FakeRedis
-
 from saltnanny import SaltReturnParser
+from saltnanny import SaltNanny
 from saltnanny import SaltRedisClient
 
 
 class SaltReturnParserTest(unittest.TestCase):
+
+    cache_config = {
+        'type': 'redis',
+        'host': 'localhost',
+        'port': 6379,
+        'db': '0'
+    }
 
     @patch('redis.Redis')
     def setUp(self, mock_redis):
@@ -43,6 +51,29 @@ class SaltReturnParserTest(unittest.TestCase):
         self.parser.cache_client.redis_instance.set('minion1:1234', '{\"return\" : [\"The function state.highstate is running as PID 1234\"]}')
         result_code = self.parser.get_return_info('minion1', '1234')
         self.assertTrue(result_code > 0)
+
+
+    @patch('redis.Redis')
+    def test_parse_last_return_with_results(self, mock_redis):
+        fake_redis = FakeRedis()
+
+        # Create and initialize Salt Nanny
+        salt_nanny = SaltNanny(self.cache_config)
+        salt_nanny.cache_client.redis_instance = fake_redis
+        salt_nanny.initialize(['minion1'])
+        salt_nanny.min_interval = 1
+
+        with open('{0}/resources/failed_highstate.json'.format(os.path.dirname(__file__)), 'r') as f:
+            json = f.read()
+
+        # Make Redis Returns available in fake redis
+        salt_nanny.cache_client.redis_instance.set('minion1:state.highstate', '6789')
+        salt_nanny.cache_client.redis_instance.set('minion1:6789', json)
+
+        return_info = salt_nanny.cache_client.get_return_by_jid('minion1','6789')
+        is_failed = self.parser.highstate_failed(return_info)
+
+        self.assertTrue(is_failed)
 
     def test_process_jids_noresults(self):
         result_code = self.parser.process_jids({}, 1)
